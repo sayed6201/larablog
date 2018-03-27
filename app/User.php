15630a07f61cad147a2e71f92995505a32f758e8
sayed6201 <sayed.ahmed620@gmail.com>
2018-03-28 01:31:52 +0600

    first commit- admin views created

---
+++ app/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password','is_active','role_id',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+//    user-role relationship 1:1
+    public function role(){
+        return $this->belongsTo('App\Role');
+    }
+}
